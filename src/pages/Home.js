da658aba69eb9b31a34bb9956625090b06db5a6b
Chaewon Lee <lcw8447@naver.com>
2023-03-05 22:10:25 +0900

    add route logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,16 @@ import React from 'react';
 import styled from 'styled-components';
 import LogoCatImage from '../assets/logocat.jpg';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    //useNavigate를 이용해 page 이동 
+    navigate('/question');
+  }
+
   return(
     <Wrapper>
     <Header>예비 집사 판별기</Header>
@@ -14,7 +22,7 @@ const Home = () => {
         <img src={LogoCatImage} className ="rounded-circle" width = {350} height = {350}/>
       </LogoImage>
       <Desc>MBTI를 기반으로 하는 나랑 잘 맞는 고양이 찾기!</Desc>
-      <Button>테스트 시작하기</Button>
+      <Button style={{fontFamily:"SimKyungha"}} onClick={handleClickButton}>테스트 시작하기</Button>
     </Contents>
     </Wrapper>
     
@@ -34,17 +42,20 @@ const Header = styled.div`
   display : flex;
   justify-content : center;
   align-items : center;
+  font-family: "SimKyungha";
 `
 const Contents = styled.div`
   display : flex;
   justify-content : center;
   align-items : center;
   flex-direction: column;
+  font-family: "SimKyungha";
 `
 
 const Title = styled.div`
   font-size : 30px;
   margin-top : 40px;
+  font-family: "SimKyungha";
 `
 
 const LogoImage = styled.div`
@@ -53,6 +64,7 @@ const LogoImage = styled.div`
 const Desc = styled.div`
   font-size : 20px;
   margin-top : 20px;
+  font-family: "SimKyungha";
 `
 
 
