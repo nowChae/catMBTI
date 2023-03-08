e7d8783539c44d28886fab17bd4fb509b5bb1c03
Chaewon Lee <lcw8447@naver.com>
2023-03-08 23:38:27 +0900

    add resultdata

---
+++ src/assets/data/resultdata.js
@@ -0,0 +1,132 @@
+import RussianBlue from '../cat/RussianBlue.jpg';
+import Siamese from '../cat/Siamese.jpg';
+import Persian from '../cat/Persian.jpg';
+import Bengal from '../cat/Bengal.jpg';
+import TurkishAngora from '../cat/TurkishAngora.jpg';
+import Abyssinian from '../cat/Abyssinian.jpg';
+import NorwegianForest from '../cat/NorwegianForest.jpg';
+import AmericanShorthair from '../cat/AmericanShorthair.jpg';
+import KoreanShorthair from '../cat/KoreanShorthair.jpg';
+import exotic from '../cat/exotic.jpg';
+import ScottishFold from '../cat/ScottishFold.jpg';
+import Munchkin from '../cat/Munchkin.jpg';
+import Ragdoll from '../cat/Ragdoll.jpg';
+import BritishShorthair from '../cat/BritishShorthair.jpg';
+import Sphynx from '../cat/Sphynx.jpg';
+import MaineCoon from '../cat/MaineCoon.jpg';
+
+export const ResultData = [
+  {
+    id: 1,
+    name: '러시안 블루', //isfj RussianBlue
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${RussianBlue}`
+  },
+  {
+    id: 2,
+    name: '샴', //infp Siamese 
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${Siamese}`
+  },
+  {
+    id: 3,
+    name: '페르시안', //infj Persian 
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${Persian}`
+  },
+  {
+    id: 4,
+    name: '뱅갈', //estp Bengal
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${Bengal}`
+  },
+  {
+    id: 5,
+    name: '터키시 앙고라', //enfj TurkishAngora
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${TurkishAngora}`
+  },
+  {
+    id: 6,
+    name: '아비시니안', //esfj Abyssinian
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${Abyssinian}`
+  },
+  {
+    id: 7,
+    name: '노르웨이 숲', //intp NorwegianForest
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${NorwegianForest}`
+  },
+  {
+    id: 8,
+    name: '아메리칸 숏헤어', //esfp AmericanShorthair
+    best: 'ESTJ', 
+    desc:'설명1',
+    image: `${AmericanShorthair}`
+  },
+  {
+    id: 9,
+    name: '코리안 숏헤어', //enfp KoreanShorthair
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${KoreanShorthair}`
+  },
+  {
+    id: 10,
+    name: '엑죠틱', //estj exotic
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${exotic}`
+  },
+  {
+    id: 11,
+    name: '스코티시 폴드', //isfp ScottishFold
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${ScottishFold}`
+  },
+  {
+    id: 12,
+    name: '먼치킨', //entj Munchkin
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${Munchkin}`
+  },
+  {
+    id: 13,
+    name: '렉돌', //istj Ragdoll
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${Ragdoll}`
+  },
+  {
+    id: 14,
+    name: '브리티시 숏헤어', //istp BritishShorthair
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${BritishShorthair}`
+  },
+  {
+    id: 15,
+    name: '스핑크스', //entp Sphynx
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${Sphynx}`
+  },
+  {
+    id: 16,
+    name: '메인쿤', //intj MaineCoon
+    best: 'ESTJ',
+    desc:'설명1',
+    image: `${MaineCoon}`
+  }
+
+]
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,9 +1,64 @@
 import React from 'react';
+//css-in-js   npm i styled-components
+import styled from 'styled-components';
+import LogoCatImage from '../assets/logocat.jpg';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import { ResultData } from '../assets/data/resultdata';
 
 const Result = () => {
+  const navigate = useNavigate();
+
   return(
-    <div>결과 화면 입니다.</div>
+    <Wrapper>
+    <Header>예비 집사 판별기</Header>
+    <Contents>
+      <Title>결과 보기</Title>
+      <LogoImage>
+        <img src={ResultData[0].image} className ="rounded-circle" width = {350} height = {350}/>
+      </LogoImage>
+      <Desc>예비 집사님과 찰떡궁합인 고양이는 {ResultData[0].name}입니다.</Desc>
+      <Button style={{fontFamily:"SimKyungha"}} onClick={navigate("/")}>테스트 다시하기</Button>
+    </Contents>
+    </Wrapper>
+    
   )
 }
 
 export default Result;
+
+const Wrapper = styled.div`
+  height : 100vh;
+  width : 100%;
+
+  `
+
+const Header = styled.div`
+  font-size : 40px;
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  font-family: "SimKyungha";
+`
+const Contents = styled.div`
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  flex-direction: column;
+  font-family: "SimKyungha";
+`
+
+const Title = styled.div`
+  font-size : 30px;
+  margin-top : 40px;
+  font-family: "SimKyungha";
+`
+
+const LogoImage = styled.div`
+  margin-top : 10px;
+`
+const Desc = styled.div`
+  font-size : 20px;
+  margin-top : 20px;
+  font-family: "SimKyungha";
+`
